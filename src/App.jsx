9ebbf57e1093d980cbc0c72521cfa8f8b2c5c74f
anilburakc <swdanilburak@gmail.com>
2025-04-09 15:16:49 +0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+
+  return (
+    <>
+      <p className="bg-amber-300">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
